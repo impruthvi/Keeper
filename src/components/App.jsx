f6c169ce35f2629927f5b74a1db6aee045ac859a
Pruthvisinh Rajput <61496236+impruthvi@users.noreply.github.com>
2020-12-12 11:23:55 +0530

    read notes using map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Header from "./Header";
-import Footer from "./footer";
+import Footer from "./Footer";
 import Note from "./Note";
+import Notes from "../notes";
 
 function App() {
   return (
     <div>
       <Header />
+      {Notes.map((noteItem) => (
+        <Note title={noteItem.title} content={noteItem.content} />
+      ))}
       <Footer />
-      <Note />
     </div>
   );
 }
