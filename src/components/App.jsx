0f17b957476b54499b5acebc2c83c37e16957007
Pruthvisinh Rajput <61496236+impruthvi@users.noreply.github.com>
2020-12-14 11:07:44 +0530

    add button added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import Notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {Notes.map((noteItem) => (
-        <Note title={noteItem.title} content={noteItem.content} />
-      ))}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
