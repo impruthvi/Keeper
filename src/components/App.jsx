7e4461bc523e50d6659e0f63f5ce058668c8c64b
Pruthvisinh Rajput <61496236+impruthvi@users.noreply.github.com>
2020-12-14 11:55:57 +0530

    Implemented the delete note functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,14 +12,28 @@ function App() {
       return [...prevNotes, newNote];
     });
   }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
 
-      {notes.map((noteItem) => {
+      {notes.map((noteItem, index) => {
         return (
-          <Note key={1} title={noteItem.title} content={noteItem.content} />
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
         );
       })}
       <Footer />
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 function CreateArea(props) {
+  
   const [note, setNote] = useState({
     title: "",
     content: ""
